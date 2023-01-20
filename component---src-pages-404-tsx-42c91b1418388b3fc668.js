9d5fb10f4dca1a5beaa1fada7e148655b3a99cd2
Thanh Pham <thanhpnm@vng.com.vn>
2023-01-20 21:14:33 +0700

    Updates

---
+++ component---src-pages-404-tsx-42c91b1418388b3fc668.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminh_thanh_portfolio=self.webpackChunkminh_thanh_portfolio||[]).push([[180],{938:function(t,e,n){n.r(e);var l=n(7294),u=n(5977);e.default=function(){return l.createElement(u.Ar,null,l.createElement(u.pQ,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-42c91b1418388b3fc668.js.map
